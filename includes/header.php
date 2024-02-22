4feba17b73fb5796a9e2aeff26bcb54679dac235
Coder0246 <daniel.hughes@cambria.ac.uk>
2024-02-22 09:42:52 +0000

    2

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
     $app = new App;
     $app->startingSession();
 
-    define("APPURL", "http://localhost/new-proj");
-    define("APPIMAGES", "http://localhost/new-proj/admin-panel/foods-admins/foods-images");
+    define("APPURL", "http://localhost/Gym-DT-A2");
+    define("APPIMAGES", "http://localhost/Gym-DT-A2/admin-panel/foods-admins/foods-images");
 
 
 ?>
@@ -18,23 +18,12 @@
     <meta content="" name="keywords">
     <meta content="" name="description">
 
-    <!-- Favicon -->
-    <link href="<?php echo APPURL; ?>/img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
-
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
+    
     <!-- Libraries Stylesheet -->
-    <link href="<?php echo APPURL; ?>/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="<?php echo APPURL; ?>/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="<?php echo APPURL; ?>/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
+   
     <!-- Customized Bootstrap Stylesheet -->
     <link href="<?php echo APPURL; ?>/css/bootstrap.min.css" rel="stylesheet">
 
@@ -43,16 +32,7 @@
 </head>
 
 <body>
-    <div class="container-xxl bg-white p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
-
-
+   
         <!-- Navbar & Hero Start -->
         <div class="container-xxl position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
